8368b3e93a4425a6acdd590c2b7c190dd579ba9e
Ben Long <113473264+longb49@users.noreply.github.com>
2022-11-03 14:49:46 -0600

    started physics

---
+++ physics.cs
@@ -0,0 +1,20 @@
+using Raylib_cs;
+using System.Numerics;
+
+class Physics
+{
+    public Vector2 position { get; set; } = new Vector2(0,0);
+    public Vector2 velocity { get; set; } = new Vector2(0,0);
+
+    virtual public void Draw()
+    {
+        // Empty for now
+    }
+
+    public void Move()
+    {
+        Vector2 newPosition = position;
+        newPosition.Y += velocity.Y;
+        position = newPosition;
+    }
+}
