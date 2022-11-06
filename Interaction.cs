9969bcade8b7843229c5bdaacbc14084a7cafc2b
Ben Long <113473264+longb49@users.noreply.github.com>
2022-11-05 22:16:11 -0600

    Update Interaction.cs

--- Interaction.cs
+++ Interaction.cs
@@ -1,16 +1,7 @@
-// using Raylib_cs;
-// using System.Numerics;
+using Raylib_cs;
+using System.Numerics;
 
-// class Collisions: Score
-// {
-//     public void nameCollisions(var TargetRectangle, var PlayerRectangle)
-//     {
-//         Raylib.DrawRectangleRec(TargetRectangle, Color.BLUE);
-//         Raylib.DrawRectangleRec(PlayerRectangle, Color.RED);
-
-//         if (Raylib.CheckCollisionRecs(PlayerRectangle, TargetRectangle)) 
-//         {
-//             Raylib.DrawText("You did it!!!!", 12, 34, 20, Color.BLACK);
-//         }
-//     }
-// }
+class Collisions : Score
+{
+    
+}
