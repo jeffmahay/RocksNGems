c5c0f49c2b8c7e5cfe17cc6a583a12c8e75cc945
Ben Long <113473264+longb49@users.noreply.github.com>
2022-11-10 13:17:31 -0700

    Done. But that one bug returns to haunt me

--- Interaction.cs
+++ Interaction.cs
@@ -2,26 +2,10 @@ using Raylib_cs;
 using System.Numerics;
 class Collisions : Score
 {
-    // public bool touchCheckGem(Gems gem, Character character)
-    // {
-    //     var posStart1 = gem.position;
-    //     var posEnd1 = gem.position + gem.velocity;
-    //     var posStart2 = character.position;
-    //     var posEnd2 = character.position + character.velocity;
-    //     var avgX = (gem.position.X + character.position.X) / 2;
-    //     var avgY = (gem.position.Y + character.position.Y) / 2;
-    //     var collPoint = new Vector2(avgX, avgY);
-    //     // return Raylib.CheckCollisionLines(posStart1, posEnd1, posStart2, posEnd2, collPoint);
-    // }
-    // public bool touchCheckRocks(Rocks gem, Character character)
-    // {
-    //     var posStart1 = gem.position;
-    //     var posEnd1 = gem.position + gem.velocity;
-    //     var posStart2 = character.position;
-    //     var posEnd2 = character.position + character.velocity;
-    //     var avgX = (gem.position.X + character.position.X) / 2;
-    //     var avgY = (gem.position.Y + character.position.Y) / 2;
-    //     var collPoint = new Vector2(avgX, avgY);
-    //     // return Raylib.CheckCollisionLines(posStart1, posEnd1, posStart2, posEnd2, collPoint);
-    // }
+    public bool checkColl(Hitbox obj, Hitbox character)
+    {
+        Rectangle objBox = new Rectangle((int)obj.position.X, (int)obj.position.Y, obj.width, obj.height);
+        Rectangle charBox = new Rectangle((int)character.position.X, (int)character.position.Y, character.width, character.height);
+        return Raylib.CheckCollisionRecs(objBox, charBox);
+    }
 }
